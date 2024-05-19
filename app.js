19e3063141bb13b4b689c5ee2e322c46622c1d0c
bleo998 <hop2k13@gmail.com>
2024-05-20 01:02:01 +0200

    vezba 2

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-// If, else if, else:
-// 1.
+// 2. Checking Day of the Week
+// - Monday - Start of the work week!
+// - Wednesday - Midweek!
+// - Friday - Almost weekend!
+// - Other day - Just another day.
 
-// Checking Age for Different Ticket Prices
-// - Child ticket: $5 (below 12 years)
-// - "Teen ticket: $7" (below 18 years)
-// - "Senior ticket
+let checkDay = "";
 
-// $10 (between 18 and 60)
-// - "Adult ticket: $10" (over 60years)
-
-let checkAge = 25;
-
-if (checkAge < 12) {
-  console.log("Child ticket: $5");
-} else if (checkAge <= 18) {
-  console.log("Teen ticket: $7");
-} else if (checkAge > 18 && checkAge <= 60) {
-  console.log("Adult ticket: $10");
+if (checkDay == "Monday") {
+  console.log("Start of the work week!");
+} else if (checkDay == "Wednesday") {
+  console.log("Midweek!");
+} else if (checkDay == "Friday") {
+  console.log("Almost weekend!");
+} else if (checkDay === "") {
+  console.log("Please choose any day of the week!!");
 } else {
-  console.log("Senior ticket");
+  console.log("Just another day.");
 }
